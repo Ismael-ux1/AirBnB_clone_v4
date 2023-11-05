20f286aec22f65c049a7fee384a8a16550886fb0
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-11-05 15:57:04 -0800

    Fetch places

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
   console.log('amenities');
   let $checkboxes = $(".amenities input[type='checkbox']");
-  let checked_amenities = [];
+  let selected_amenities = [];
 
   $checkboxes.on('change', function () {
     amenity_id = $(this).data('id');
     amenity_name = $(this).data('name');
 
     if ($(this).is(':checked')) {
-      checked_amenities.push({
+      selected_amenities.push({
         'id': amenity_id,
         'name': amenity_name
 
       });
     }else {
-      checked_amenities = checked_amenities.filter(function (amenity) {
+      selected_amenities = selected_amenities.filter(function (amenity) {
         return amenity.id != amenity_id;
       });
     }
     $('.amenities h4').empty();
-    for (let index = 0; index < checked_amenities.length; index++) {
-      if (checked_amenities.length == 1 || index == checked_amenities.length - 1) {
-        $('.amenities h4').append(checked_amenities[index].name);
+    for (let index = 0; index < selected_amenities.length; index++) {
+      if (selected_amenities.length == 1 || index == selected_amenities.length - 1) {
+        $('.amenities h4').append(selected_amenities[index].name);
       }else {
-        $('.amenities h4').append('' + checked_amenities[index].name + ', ');
+        $('.amenities h4').append('' + selected_amenities[index].name + ', ');
       }
     }
-    if (checked_amenities.length >= 3) {
+    if (selected_amenities.length >= 3) {
       $('.amenities h4').addClass('text-ellipsis');
     } else {
       $('.amenities h4').removeClass('text-ellipsis');
